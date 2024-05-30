d6f09bf3faf2cdabf1bf8c790affe799317a4a88
Moritz Neeb <nt4u@kpvn.de>
2024-05-31 00:19:20 +0200

    add first ugly lexer (but it passes test suite :))

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Args, Parser};
+use regex::Regex;
 use std::fs;
 use std::path::PathBuf;
 use std::process::{self, Command};
@@ -27,6 +28,76 @@ struct Step {
     codegen: bool,
 }
 
+#[derive(Debug)]
+enum Token {
+    Identifier(String),
+    Constant(u64),
+    Keyword(String),
+    OpenParen,
+    CloseParen,
+    OpenBrace,
+    CloseBrace,
+    Semicolon,
+}
+fn lexer(text: String) -> Vec<Token> {
+    // while input isn't empty:
+    //   if input starts with whitespace:
+    //     trim whitespace from start of input
+    //   else:
+    //     find longest match at start of input for any regex in Table 1-1
+    //     if no match is found, raise an error
+    //     convert matching substring into a token
+    //     remove matching substring from start of input
+    let mut token = vec![];
+    let mut input = text.as_str();
+    while !input.is_empty() {
+        let char = input.chars().next().expect("Should have had a character");
+
+        if char.is_whitespace() {
+        } else if char == '(' {
+            token.push(Token::OpenParen);
+        } else if char == ')' {
+            token.push(Token::CloseParen);
+        } else if char == '{' {
+            token.push(Token::OpenBrace);
+        } else if char == '}' {
+            token.push(Token::CloseBrace);
+        } else if char == ';' {
+            token.push(Token::Semicolon);
+        } else {
+            let keyword = Regex::new(r"^(void|int|return)\b").unwrap();
+            let constant = Regex::new(r"^([0-9]+)\b").unwrap();
+            let identifier = Regex::new(r"^([a-zA-Z_]\w*)\b").unwrap();
+            if constant.is_match(input) {
+                let caps = constant.captures(input).unwrap();
+                let matched_const = caps.get(0).unwrap().as_str();
+                input = &input[matched_const.len()..];
+                token.push(Token::Constant(matched_const.parse().unwrap()));
+                continue;
+            } else if identifier.is_match(input) {
+                if keyword.is_match(input) {
+                    let caps = keyword.captures(input).unwrap();
+                    let matched_keyword = caps.get(0).unwrap().as_str();
+                    input = &input[matched_keyword.len()..];
+                    token.push(Token::Keyword(matched_keyword.into()));
+                    continue;
+                }
+                let caps = identifier.captures(input).unwrap();
+                let matched_identifier = caps.get(0).unwrap().as_str();
+                input = &input[matched_identifier.len()..];
+                token.push(Token::Identifier(matched_identifier.into()));
+                continue;
+            } else {
+                println!("ERROR");
+                process::exit(1);
+            }
+        }
+
+        input = &input[1..];
+    }
+    token
+}
+
 fn main() {
     let cli = Driver::parse();
     println!("Starting to compile {}", cli.path.display());
@@ -49,7 +120,10 @@ fn main() {
         .unwrap();
     println!("Preprocess finished with: {prep}");
 
-    println!("Compiling! (..not...)");
+    println!("Lexing! (..not...)");
+    let text = fs::read_to_string(prep_file).expect("Failed to read input file.");
+    dbg!(lexer(text));
+
     if cli.step.lex {
         println!("Wrapping it up after Lexing.");
         fs::remove_file(prep_file).expect("Could not remove preprocessed file.");
